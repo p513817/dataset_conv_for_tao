4dfcedf429e17e7c3452deafbad000081e907db0
p513817 <p513817@gmail.com>
2021-10-26 09:06:43 +0800

    new version

--- README.md
+++ README.md
@@ -16,57 +16,79 @@ convert dataset's format for NVIDIA TAO Toolkit.
 2. modify `map_table.json`
     ```json
     {
-        "in_type":"yolo",
-        "out_type":"kitti",
+        "dataset":"/path/to/your/dataset",
+        "mode_option":["convert", "only-show"],
+        "mode":"mode option",
+        "in_type":"input type",
+        "out_type":"output type",
+        "sample_path":"./sample.png",
+        "sample_grid":"3x3",
         "map_class":{
-            "origin":"changed name"
+            "0":"name you want to replace",
+            "1":"name you want to replace"
         }
     }
     ```
+    * dataset: path of your dataset.
+    * mode: choice a mode from mode_option.
     * in_type: the dataset's format which you want to convert.
     * out_type: final output format of dataset.
-    * map_class: you can change the class's name via setup `item` like sample.
+    * sample_path: path of sample which generate after converting.
+    * sample_gird: you can define the grid of the sample image.
+    * map_class: you can change the class's name via setup `map_classes` like the sample.
+
 3. convert dataset's format
     ```bash
-    $ task='/path/to/your/dataset'
-    $ python3 fmt_converter.py -i ${task} -m ./map_table.json 
+    $ python3 fmt_converter.py
     ```
 
 ## Demo
- ```json
- {
-     "in_type":"yolo",
-     "out_type":"kitti",
-     "map_class":{
-         "0":"usb",
-         "1":"usb"
-     }
- }
- ```
-```bash
-$ python3 fmt_converter.py -i ./usb/ -m ./map_table.json 
-* Convert from 'yolo' to 'kitti'
-* Convert Classes Map: {'0': 'usb', '1': 'usb'}
+1. Convert
+    ```json
+    {
+        "dataset":"mask",
+        "mode_option":["convert", "only-show"],
+        "mode":"convert",
+        "in_type":"yolo",
+        "out_type":"kitti",
+        "sample_grid_path":"./sample.jpg",
+        "sample_grid":"3x5",
+        "map_class":{
+            "0":"mask",
+            "1":"no-mask",
+            "2":"abnormal"
+        }
+    }
+    ```
 
-[ Start Convert ]
-100%|█████████████████████| 51/51 [00:00<00:00, 6933.86it/s]
-Press 'q' and 'esc' to leave, 's' to save in sample.png ... 
-Saved Image ('./sample.png') 
-```
+    ```bash
+    $ python3 fmt_converter.py
 
-![image](./figures/sample.png)
+    [ Start Convert ]
+    100%|█████████████████████| 51/51 [00:00<00:00, 6933.86it/s]
+    Press 'q' and 'esc' to leave, 's' to save in sample.png ... 
+    Saved Image ('./sample.png') 
+    ```
 
-## Help
-```bash
-$ python3 fmt_converter.py --help
-usage: fmt_converter.py [-h] [-i INPUT] [-m MAP] [-n NUM] [-s SHOW] [--only-show]
+    ![image](./figures/sample.jpg)
 
-optional arguments:
-  -h, --help            show this help message and exit
-  -i INPUT, --input INPUT
-                        input files folder
-  -m MAP, --map MAP     mapping-format file
-  -n NUM, --num NUM     number of samples
-  -s SHOW, --show SHOW  show samples
-  --only-show           only show samples, not convert
-```
+2. Only-Show
+    Dependence on "out_type"
+    ```json
+    {
+        "dataset":"mask",
+        "mode_option":["convert", "only-show"],
+        "mode":"only-show",
+        "in_type":"yolo",
+        "out_type":"yolo",
+        "sample_grid_path":"./sample_org.jpg",
+        "sample_grid":"3x5",
+        "map_class":{
+            "0":"mask",
+            "1":"no-mask",
+            "2":"abnormal"
+        }
+    }
+    ```
+
+    ![image](./figures/sample_org.jpg)
--- fmt_converter.py
+++ fmt_converter.py
@@ -3,50 +3,34 @@
 
 ###################################################################
 # Import Lib
-import argparse
 import os
 from tqdm import tqdm
-import json
 import cv2
 import glob
 import numpy as np
 from utils import check_path, bbox_yolo2norm, parse_json, dset_format, name_mapping, dset_check_parse, draw_bbox
 
-def init_args():
-    # Initial argparse 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--input", help="input files folder")
-    parser.add_argument("-m","--map", help="mapping-format file")
-    parser.add_argument("-n","--num", default=9, type=int, help="number of samples")
-    parser.add_argument("-s","--show", default=True, help="show samples")
-    parser.add_argument("--only-show", action="store_true", help="only show samples, not convert")
-    args = parser.parse_args()
-    return args
-
-###################################################################
-# 初始化
-
-# 初始化 argparse
-args=init_args()
-
-# 檢查輸入的位置是否正確、是否存在
-check_path(args.input)
 
 # 取得 map_table.json 的資訊
 content_temp=[]                                         # 用於更換標籤內容的變數
-in_type, out_type, map_class = parse_json(args.map)    # 解析 json 檔案 
-print(f"* Convert from '{in_type}' to '{out_type}'")
-print(f"* Convert Classes Map: {map_class}")
-if in_type != "yolo": print("* Warning: Default format is 'yolo'")
+dataset, mode, in_type, out_type, sample_grid, sample_grid_path, map_class = parse_json('./map_table.json')    # 解析 json 檔案 
+
+# 檢查輸入的位置是否正確、是否存在
+check_path(dataset)
 
 # 取得所有標籤檔
-txt_files=glob.glob(f'{args.input}/*.txt')
+txt_files=glob.glob(f'{dataset}/*.txt')
 
 # 取得所有圖檔，使用第一張取得圖片大小
-img_files=glob.glob(f'{args.input}/*.jpg')
+img_files=glob.glob(f'{dataset}/*.jpg')
 img_sample=cv2.imread(img_files[0])             # 讀取樣本
-height, width, channel= img_sample.shape        # 取得高寬通道
+        # 取得高寬通道
 frames, row_frames=[],[]                        # 為了顯示用的
+
+# 定義視窗
+NAME = "SAMPLES"
+cv2.namedWindow(NAME, cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
 
 ###################################################################
 # 開啟標籤檔並將內容按照 map_table 上的參數進行偵測與替代
@@ -56,20 +40,21 @@ frames, row_frames=[],[]                        # 為了顯示用的
 #       e.g.將 bbox 從 yolo 轉換成 kitti 後，從 map_class 中去檢查是否有相同的並且更換
 #   [4] 將剛剛更新後的內容 ( content_temp ) 寫入標籤檔
 
-if not args.only_show:
+if mode != "only-show":
 
     print("\n[ Start Convert ]")
     # 將所有的標籤檔逐一讀取
-    for txt in tqdm(txt_files):
+    for img_path in tqdm(img_files):
         
-        # 如果讀取到 classes.txt 就跳過
-        if txt == os.path.join(args.input, "classes.txt"): continue
+        height, width, _ = cv2.imread(img_path).shape
+
+        label_path=os.path.splitext(img_path)[0]+'.txt'
 
         # 每讀取一張，就刷新 content_temp 以免寫到之前的內容
         content_temp=[]
 
         # [1]
-        with open(txt,'r') as label:
+        with open(label_path,'r') as label:
             for line in label.readlines():
                 # [2]
                 raw = (line.rstrip('\n').split(" "))    
@@ -90,7 +75,7 @@ if not args.only_show:
                     content_temp.append( trg_format(name_mapping(classes, map_class), cx, cy, w, h) )
         
         # [4]
-        with open(txt,'w') as label:
+        with open(label_path,'w') as label:
             for cnt in content_temp:
                 label.write(cnt+'\n')
 
@@ -104,54 +89,43 @@ if not args.only_show:
 #   [6] 如果儲存到平方次數，則進行儲存，e.g.num=9, 則每合併3張就儲存一次
 #   [7] 將過水平合併的圖片，進行垂直合併
 #   [8] 一些輸出的動作
+  
+row, col = [ int(x) for x in sample_grid.split("x")]
+sample_id_list = np.random.choice(len(img_files), size=row*col, replace=False)
 
-if args.show:
-    # [1]    
-    for idx, img_id in enumerate(np.random.randint(len(img_files), size=args.num)):
-        # [2]
-        img_path = img_files[img_id]
-        name, ext = os.path.splitext(img_path)
-        frame = cv2.imread(img_path)
-        # [3]
-        with open(name+'.txt', 'r') as label:
-            for line in label.readlines():
-                raw=line.rstrip('\n').split(" ")
-                classes=raw[0]
+avg_size = int(np.average([cv2.imread(img_files[idx]).shape[1] for idx in sample_id_list]))
 
-                if out_type=="kitti":
-                    x1,y1,x2,y2=map(lambda x:int(float(x)), raw[4:8])
-                elif out_type=="yolo":
-                    x1,y1,x2,y2=map(lambda x:int(x), bbox_yolo2norm(raw[1], raw[2], raw[3], raw[4], frame.shape[1], frame.shape[0]))
-
-                # [4]
-                cv2.putText(frame, f"{img_path}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
-                frame = draw_bbox(frame, classes, (x1,y1,x2,y2) )
-
-        # [5]
-        row_frames = frame if (idx)%(np.sqrt(args.num))==0 else np.hstack((row_frames, frame))
-        # [6]
-        if ((idx+1)%(np.sqrt(args.num)))==0:
-            frames.append(row_frames)
-            row_frames=[]
-    # [7]
-    frames=np.vstack((frames))
-
-    # [8]
-    cv2.namedWindow('Sample', cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty("Sample", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN);
-    cv2.imshow('Sample', frames)
-    print("Press 'q' and 'esc' to leave, 's' to save in sample.png ... ")
-    while(1):
-        key=cv2.waitKey(1)
-        if key in [ord('q'), 27]: 
-            print('Quit')
-            break
-        elif key == ord('s'): 
-            print("Saved Image ('./sample.png') ")
-            cv2.imwrite('./sample.png', frames)
-            break
-        else: 
-            pass
+for idx, img_id in enumerate(sample_id_list):
     
-    cv2.destroyAllWindows()
+
+    img_path = img_files[img_id] 
+    label_path=os.path.splitext(img_path)[0]+'.txt'
+
+    frame = draw_bbox(img_path, label_path, out_type)
+    frame = cv2.resize(frame, (avg_size,avg_size))
+    frame = cv2.putText(frame, f"{img_path}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+
+    row_frames = frame if (idx)%(col)==0 else np.hstack((row_frames, frame))
     
+    if ((idx+1)%(col))==0:
+        frames.append(row_frames)
+        row_frames=[]
+
+frames=np.vstack((frames))
+
+cv2.imshow(NAME, frames)
+
+print(f"Press 'q' and 'esc' to leave, 's' to save in {sample_grid_path} ... ")
+while(1):
+    key=cv2.waitKey(1)
+    if key in [ord('q'), 27]: 
+        print('Quit')
+        break
+    elif key == ord('s'): 
+        print(f"Saved Image ('{sample_grid_path}') ")
+        cv2.imwrite(sample_grid_path, frames)
+        break
+    else: 
+        pass
+
+cv2.destroyAllWindows()
